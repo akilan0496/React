7023714d0f259c5544f931bd6981925eea99b0a8
Akilan Mohan <akilan.mohan@sc.com>
2017-03-06 11:46:13 +0800

    Commiting React Stateless, Component example

---
+++ react-stateless/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GridLayout from './component/Grid-Layout.js'
+
+ReactDOM.render(<GridLayout/>, document.getElementById('app'));
---
+++ react-stateless/component/Grid-Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header.js';
+import Content from './Content.js';
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        This my Testing
+        heheheeh
+        <Header/>
+        <Content/>
+      </div>
+    );
+  }
+});
